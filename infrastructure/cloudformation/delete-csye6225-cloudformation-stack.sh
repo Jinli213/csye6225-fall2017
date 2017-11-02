d2ad2a37f0ca5df7822b79515eac2470f35adb97
jinli <li.jin3@husky.neu.edu>
2017-11-01 22:17:15 -0400

    update

--- infrastructure/cloudformation/delete-csye6225-cloudformation-stack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo  "Input the stack name which you want to terminate"
-read name
-aws cloudformation delete-stack --stack-name $name
--- infrastructure/scripts/terminate-ec2-instance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo  "Input the id of instance which you want to terminate"
-read id
-aws ec2 terminate-instances --instance-ids $id
-aws ec2 delete-security-group --group-name csye6225-fall2017-webapp
