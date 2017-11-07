41adddd71e62a2ad6a3689f82c0a71585aa3753a
zcy <zhao.chenya@husky.neu.edu>
2017-11-07 18:01:28 -0500

    new

---
+++ src/test/java/com/csye6225/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.csye6225.demo;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class DemoApplicationTests {
+
+
+  @Test
+  public void contextLoads() {
+    assertEquals(11,11);
+
+  }
+
+}
